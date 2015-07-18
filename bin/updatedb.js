8aeac9a6945a492ad7782aa0da0447553aeb9e9a
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2015-07-19 00:36:42 +0200

    fixed error

--- bin/updatedb.js
+++ bin/updatedb.js
@@ -22,8 +22,8 @@ var async = require('async');
 var winston = require('winston');
 
 async.series([
-    function(callback){ blocket.scrape(callback); },
-    function(callback){ db.clean(callback); }
+    function(callback){ db.clean(callback); },
+    function(callback){ blocket.scrape(callback); }
 ], function(err, results){
     winston.log("info", "finished.");
 });
