0958848a1e8656e3b56dc96f13f437021c4b9d2c
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2015-08-04 19:23:45 +0200

    fixed express static file serving under nginx

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/blocket', express.static('public'));
 
 app.use('/', routes);
 app.use('/users', users);
